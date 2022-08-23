6377f441630eaccbde6dc056994a842bc156eae0
Shweta Mishra <shmis2004@gmail.com>
2022-08-24 01:08:37 +0530

    delete button function

--- indx.js
+++ indx.js
@@ -1,6 +1,6 @@
 
 let myLeads = []
-
+let oldLeads = []
 //turn string into an array
 //myLeads = JSON.parse(myLeads)
 
@@ -14,46 +14,72 @@ let myLeads = []
 const newButton = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el") 
 const ulEl = document.getElementById("ul-el")
+const tabButton = document.getElementById("tab-btn")
+const delbtn =  document.getElementById("del-el")   
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-console.log(leadsFromLocalStorage)
+
+//console.log(leadsFromLocalStorage)
 //localStorage.setItem("myName", "Shweta")
 //let name = localStorage.getItem("myName")
 //console.log(name)
 
+
 if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-newButton.addEventListener("click", function(){
-//    myLeads.push("www.awesomelead.com")
+tabButton.addEventListener("click", function(){
+    chrome.tabs.query({active: true},function(tabs){
+          myLeads.push(tabs[0].url)
+          localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+
+       })  
+       })
+       
+
+     
+
+function render(leads){
+
+    let listItems = ""
+    
+    for( let i=0 ; i<leads.length ; i++){
+      //ulEl.innerHTML += "<li>" +  myLeads[i] + "</li>"
+       // listItems  += "<li><a target ='_blank' href ='' + myLeads[i] + "'>" +  myLeads[i] + "</a>"
+       listItems  += `
+          <li>
+         <a target ='_blank' href ="${leads[i]}">
+         ${leads[i]}
+         </a>
+         </li>
+       `
+    } 
+    ulEl.innerHTML = listItems
+    }
+
+    delbtn.addEventListener("dblclick", function(){
+    
+        localStorage.clear()
+        myLeads = []
+        render(myLeads)
+    })
+    
+       
+
+    newButton.addEventListener("click", function(){
      myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-     renderLeads()
+     inputEl.value = ""
+     localStorage.setItem("myLeads", JSON.stringify(myLeads))
+     render(myLeads)
 })
 
 // const is used as let in modern javascript
 // but const cannot be reassign
 
-function renderLeads(){
 
-let listItems = ""
-
-for( let i=0 ; i<myLeads.length ; i++){
-  //ulEl.innerHTML += "<li>" +  myLeads[i] + "</li>"
-   // listItems  += "<li><a target ='_blank' href ='' + myLeads[i] + "'>" +  myLeads[i] + "</a>"
-   listItems  += `
-      <li>
-     <a target ='_blank' href ="${myLeads[i]}">
-     ${myLeads[i]}
-     </a>
-     </li>
-   `
-} 
-ulEl.innerHTML = listItems
-}
 
 //innertext 
 
